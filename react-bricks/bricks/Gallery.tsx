7dbb03724fd79c41b3e799b7f6547b7dbf96eaa6
damian@tripbuddi.com <damian@tripbuddi.com>
2022-12-31 15:28:32 +0700

    The Galley title is now editable in the CMS

--- react-bricks/bricks/Gallery.tsx
+++ react-bricks/bricks/Gallery.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { types, Repeater } from 'react-bricks/frontend'
+import { types, Text, Repeater } from 'react-bricks/frontend'
 
 const Gallery: types.Brick = () => {
   return (
     <div className="max-w-5xl mx-auto py-8 px-4">
-      <h1 className="text-4xl font-bold text-center">Gallery</h1>
+      <Text
+        propName="title"
+        renderBlock={({ children }) => <h1 className="text-2xl font-boldtext-4xl font-bold text-center">{children}</h1>}
+        placeholder="Type a title..."
+        // multiline={false} : boolean
+        // softLineBreak={false} : boolean
+    />
       <div className="sm:grid grid-cols-3 gap-5">
         <Repeater propName="thumbnails" />
       </div>
